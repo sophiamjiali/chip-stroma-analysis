51254a733db9e145d60742e6eb51bc1ef1cc0b3e
Sophia Li <sophiamjiali@gmail.com>
2026-06-17 15:29:31 -0400

    Added preproc slurm script

---
+++ slurm/run_preprocess.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --output=/cluster/home/t144807uhn/logs/chip-stroma-analysis/%x_%j.out
+#SBATCH --error=/cluster/home/t144807uhn/logs/chip-stroma-analysis/%x_%j.err
+#SBATCH --time=6:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=24G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=sophiamjia.li@mail.utoronto.ca
+
+# Make the project-specific logs directory
+mkdir -p /cluster/home/t144807uhn/logs/chip-stroma-analysis
+
+# Activate the virtual environment
+module load python3/3.12.11
+source /cluster/home/t144807uhn/envs/chip-stroma-env/bin/activate
+
+# Ensure that all commands resolve back to the proper root directory
+cd /cluster/home/t144807uhn/chip-stroma-analysis
+
+# Avoid PyTorch Lightning SLURM mis-detection issues
+unset SLURM_NTASKS
+unset SLURM_JOB_NAME
+
+export CUDA_VISIBLE_DEVICES=""
+export PYTORCH_ENABLE_MPS_FALLBACK=0
+
+echo "=========================================="
+echo "Job ID:     $SLURM_JOB_ID"
+echo "Job Name:   $1"
+echo "Node:       $SLURMD_NODENAME"
+echo "GPU:        $CUDA_VISIBLE_DEVICES"
+echo "Start:      $(date)"
+echo "=========================================="
+
+srun python scripts/preprocess.py \
+    --config_dir configs/hpc
+
+echo "=========================================="
+echo "End: $(date)"
+echo "=========================================="

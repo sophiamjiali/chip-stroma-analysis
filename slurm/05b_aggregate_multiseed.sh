ed3f6e0fa4a7bdcaf9fb6ed186163b885448aa42
Sophia Li <sophiamjiali@gmail.com>
2026-07-24 13:50:00 -0400

    Bugfix for partition targetting

--- slurm/05b_aggregate_multiseed.sh
+++ slurm/05b_aggregate_multiseed.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #SBATCH --output=/cluster/home/t144807uhn/logs/chip-stroma-analysis/multiseed/%x/aggregate_%j.out
 #SBATCH --error=/cluster/home/t144807uhn/logs/chip-stroma-analysis/multiseed/%x/aggregate_%j.err
-#SBATCH --account=kumargroup_gpu
-#SBATCH -p cpu
 #SBATCH --time=00:15:00
+#SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=4G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=sophiamjia.li@mail.utoronto.ca
 
 source /cluster/home/t144807uhn/envs/chip-stroma-env-gpu/bin/activate
 cd /cluster/home/t144807uhn/chip-stroma-analysis

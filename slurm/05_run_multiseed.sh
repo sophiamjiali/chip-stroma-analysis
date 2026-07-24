eacc26eb76e8be9511b613959c80cbc9c1a14f7b
Sophia Li <sophiamjiali@gmail.com>
2026-07-24 13:54:53 -0400

    ditto

--- slurm/05_run_multiseed.sh
+++ slurm/05_run_multiseed.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 TOP_K=2; N_SEEDS=3
+cd /cluster/home/t144807uhn/chip-stroma-analysis/slurm
+
 ARRAY_JOBID=$(sbatch --parsable --array=0-$((TOP_K*N_SEEDS-1)) --job-name=v6 05a_submit_multiseed.sh v6)
 sbatch --dependency=afterok:$ARRAY_JOBID --job-name=v6 05b_aggregate_multiseed.sh v6

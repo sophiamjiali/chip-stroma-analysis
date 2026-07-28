759afd03597cc3f1e2c8c73110187a67cc43e71b
Sophia Li <sophiamjiali@gmail.com>
2026-07-28 14:12:36 -0400

    Reduced memory and CPU allocation for inference tests

--- slurm/07_run_inference.sh
+++ slurm/07_run_inference.sh
@@ -4,10 +4,10 @@
 #SBATCH --account=kumargroup_gpu
 #SBATCH -p gpu
 #SBATCH --gres=gpu:1
-#SBATCH --time=12:00:00
+#SBATCH --time=04:00:00
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=12
-#SBATCH --mem=24G
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=20G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=sophiamjia.li@mail.utoronto.ca
 

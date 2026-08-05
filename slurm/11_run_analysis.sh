c0ec6d4a503c8f0f6f659ab6024cf8fe5b616251
Sophia Li <sophiamjiali@gmail.com>
2026-08-05 15:37:35 -0400

    Add slurm submission script

--- slurm/11_run_analysis.sh
+++ slurm/11_run_analysis.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-#SBATCH --account=kumargroup_gpu
-#SBATCH -p gpu
-#SBATCH --gres=gpu:1
-#SBATCH --time=04:00:00
+#SBATCH --time=0:30:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=20G
+#SBATCH --mem=8G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=sophiamjia.li@mail.utoronto.ca
 
@@ -22,10 +19,9 @@ source ${PROJECT_ROOT}/slurm/00_setup_env.sh
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "Job ID:     $SLURM_JOB_ID"
 echo "Node:       $SLURMD_NODENAME"
-echo "GPU:        $CUDA_VISIBLE_DEVICES"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
-python -u scripts/10_quantify.py \
+python -u scripts/11_analysis.py \
     --config_dir $CONFIG_DIR \
     --version $VERSION
 

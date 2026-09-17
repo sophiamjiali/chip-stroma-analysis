af3d1b5412ff3086c1333e35c2bb7234526f8a4d
Sophia Li <sophiamjiali@gmail.com>
2026-09-17 16:13:35 -0400

    Add stitching submission script

---
+++ slurm/12_stitch_masks.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=16G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=sophiamjia.li@mail.utoronto.ca
+
+set -euo pipefail
+
+# Extract command-line arguments for clarity
+PROJECT_ROOT=$1
+VERSION=$2
+
+# Initialize the standardized environment
+source ${PROJECT_ROOT}/.env
+source ${PROJECT_ROOT}/slurm/00_setup_env.sh
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Job ID:     $SLURM_JOB_ID"
+echo "Node:       $SLURMD_NODENAME"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+python -u scripts/12_stitch_masks.py \
+    --config_dir $CONFIG_DIR \
+    --version $VERSION
+
+# [END]
